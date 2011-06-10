5ff9f19360d69cafda009fe8c8f632ebf97d9faa
Xtra <alexandrepor@gmail.com>
2011-06-10 21:25:21 +0100

    Commited

---
+++ src/Xtra/PlayerList/PlayerHideCommandExecutor.java
@@ -0,0 +1,57 @@
+package Xtra.PlayerList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.bukkit.util.config.Configuration;
+//import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+
+public class PlayerHideCommandExecutor implements CommandExecutor {
+
+	private PlayerList plugin;
+	public Configuration config;
+	public boolean tellAll;
+	public boolean hideNumbers;
+	public List<String> hidden = new ArrayList<String>();	
+	public PlayerHideCommandExecutor(PlayerList plugin) {
+		this.plugin = plugin;
+	}
+
+	
+	
+	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+		
+		if (commandLabel.equalsIgnoreCase("phide")){
+						
+			if (sender instanceof Player)
+			{
+				System.out.println("Started");
+				String toadd;
+				toadd = ((Player) sender).getDisplayName();
+				plugin.hidden.add(toadd);
+		    	plugin.saveConfig();
+				System.out.println("Added " + toadd + "to hidden list");
+				return true;
+			}
+			else
+			{
+				System.out.println("Command can only be used by a player");
+				return true;
+			}
+			
+			
+		}
+		return true;
+		
+}
+
+
+
+}
